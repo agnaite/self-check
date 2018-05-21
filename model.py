ebf7524a03f075f79c9a165ae5f182d0ee3cf131
Agne Klimaite <agnaite@me.com>
2018-05-21 16:00:45 -0700

    add ability to create new self-check

--- model.py
+++ model.py
@@ -7,14 +7,14 @@ db = SQLAlchemy()
 
 
 class SelfCheckSession(db.Model):
-    """A Self-Check."""
+    """A self-check session."""
 
     __tablename__ = "self_check_sessions"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     timestamp = db.Column(db.DateTime())
 
-    answers = db.relationship("Answer", backref="self_check_session")
+    answers = db.relationship("Answer", backref="self_check_sessions")
 
 
 class QuestionDefinitionSet(db.Model):
@@ -27,37 +27,37 @@ class QuestionDefinitionSet(db.Model):
     active = db.Column(db.Boolean, default=True)
 
 class QuestionDefinition(db.Model):
-    """A Question."""
+    """A question definition."""
 
     __tablename__ = "question_definitions"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     question_definition_set_id = db.Column(db.ForeignKey("question_definition_sets.id"))
     body = db.Column(db.Text, nullable=False)
-    response_type_id = db.Column(db.String(3), nullable=False)
 
-    question_definition_set = db.relationship("QuestionDefinitionSet", backref="question_definitions")
+    question_definition_sets = db.relationship("QuestionDefinitionSet", backref="question_definitions")
 
 
 class Answer(db.Model):
-    """An Answer."""
+    """An answer to a question."""
 
     __tablename__ = "answers"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     self_check_session_id = db.Column(db.ForeignKey("self_check_sessions.id"))
-    question_definition_id = db.Column(db.Integer, db.ForeignKey("question_definitions.id"))
+    question_definition_id = db.Column(db.ForeignKey("question_definitions.id"))
     response = db.Column(db.String(240), nullable=False)
     
-    question_definition = db.relationship("QuestionDefinition", backref="answers")
+    question_definitions = db.relationship("QuestionDefinition", backref="answers")
 
 def connect_to_db(app):
     """Connect the database to the Flask app."""
 
     # Configure to use our database
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///selfchecks"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    app.config["SQLALCHEMY_ECHO"] = True 
 
-    app.config["SQLALCHEMY_ECHO"] = True
     db.app = app
     db.init_app(app)
 
@@ -67,6 +67,7 @@ if __name__ == "__main__":
     from server import app
 
     connect_to_db(app)
+    db.drop_all()
     db.create_all()
  
     print("Connected to DB.")
--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session, redirect
 from flask_assets import Environment
 from jinja2 import StrictUndefined
 from datetime import datetime
 
-from model import connect_to_db, db
+from model import connect_to_db, db, QuestionDefinition, QuestionDefinitionSet, Answer, SelfCheckSession
+
 
 app = Flask(__name__)
 
@@ -14,6 +15,7 @@ assets = Environment(app)
 app.jinja_env.undefined = StrictUndefined
 assets.url = app.static_url_path
 app.config["ASSETS_DEBUG"] = True
+app.config["ENV"] = "development"
 app.secret_key = "abz" 
 
 
@@ -22,8 +24,17 @@ app.secret_key = "abz"
 @app.route("/")
 def index_page():
     """Show index page."""
-
+    print("APP CONFIG ", app.config)
     return render_template("index.html")
+
+@app.route("/self-checks")
+def show_all_self_checks():
+    """Show all active self-checks."""
+    
+    self_checks = QuestionDefinitionSet.query.filter_by(active=True).all()
+    
+    return render_template("self_checks.html",
+                           self_checks=self_checks)
 
 @app.route("/new-self-check")
 def add_new_self_check():
@@ -32,11 +43,22 @@ def add_new_self_check():
     #TODO: grab form data
     # add instance to self check table
 
-    return redirect("/")
+    return render_template("/new_self_check.html")
+
+@app.route("/submit-self-check", methods=["POST"])
+def submit_new_self_check():
+    """Submit new self-check."""
+    
+    name = request.form.get("name")
+
+    self_check = QuestionDefinitionSet(name=name, active=True)
+    db.session.add(self_check)
+    db.session.commit()
+
+    return redirect("/self-checks")
 
 
 if __name__ == "__main__":
 
     connect_to_db(app)
-
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
